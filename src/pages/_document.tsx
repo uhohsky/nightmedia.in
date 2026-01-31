a1e203f51f07987c3f4c69164226f9fd6112783e
uhohsky <uhohsky4@gmail.com>
2026-01-31 03:21:40 +0000

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,1 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Search Console Verification */}
+        <meta
+          name="google-site-verification"
+          content="abc123XYZ987"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
 
