f5ef64966e7d9f1aece189318808ce7e7cf0a5f6
Sky <uhohsky4@gmail.com>
2026-02-07 15:58:24 +0000

    Fix Safari black screen by correcting import order

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+// Safari detection & fix (AFTER imports)
+const isSafari =
+  /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (isSafari) {
+  document.documentElement.classList.add("safari");
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
+
